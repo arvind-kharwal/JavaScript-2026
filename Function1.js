191a6412476fd511819f8fafbeac637b49bbb571
Arvind Kharwal <karvind629@gmail.com>
2026-07-14 11:37:58 +0530

    14 July 2026

--- Function1.js
+++ Function1.js
@@ -1,7 +1,19 @@
-function say(message){
-    console.log('This is:'+message);
-    console.log('This is:',{message});
-    console.log(`This is ${message}`);
-}
-say("Hello JavaScript")
+// function say(message){
+//     console.log('This is:'+message);
+//     console.log('This is:',{message});
+//     console.log(`This is ${message}`);
+// }
+// say("Hello JavaScript")
 
+
+function compare(a, b) {
+    if (a > b) {
+        return -1;
+    } else if (a < b) {
+        return 1;
+    }
+    return 0;
+}
+
+let res = compare(10,10);
+console.log(res);
