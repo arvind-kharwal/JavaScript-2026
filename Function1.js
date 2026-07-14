3bc86e3d052978cb8a814991b63dd09b27e343d4
Arvind Kharwal <karvind629@gmail.com>
2026-07-14 09:57:39 +0530

    14 July 2026

--- Function1.js
+++ Function1.js
@@ -1,12 +1,7 @@
-function myName(){
-    console.log('Hello Arvind');
-}
-
 function say(message){
     console.log('This is:'+message);
     console.log('This is:',{message});
     console.log(`This is ${message}`);
 }
-myName();
 say("Hello JavaScript")
 
