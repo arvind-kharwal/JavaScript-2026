925b965c059105f830da5a68814bc2d35916f2c3
Arvind Kharwal <karvind629@gmail.com>
2026-07-14 10:19:23 +0530

    14 July 2026

--- Function2.js
+++ Function2.js
@@ -1,6 +1,22 @@
-function display(m){
-    console.log(m);
+//function hoisting
+msg1("Yuvaan");
+
+function msg1(m1){
+    console.log('Hello '+m1);
 }
 
-let msg = display("Hello")
-console.log(msg);
+
+
+// function display(m){
+//     return "Hello " + m;
+// }
+
+// let msg = display("Arvind")
+// console.log(msg);
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// let res = add(10,20);
+// console.log("The result is: "+res);
