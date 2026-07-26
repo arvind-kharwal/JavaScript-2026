e98f51a4d99dbbb4bb2d10e5877e684e87c66f2d
Arvind Kharwal <karvind629@gmail.com>
2026-07-26 22:26:03 +0530

    26 July 2026

--- Objects/Object2.js
+++ Objects/Object2.js
@@ -4,10 +4,12 @@ const student = {
     laptop: {
         cpu:'i7',
         ram:8,
-        brand:'Dell',
+        brand1:'Dell',
     }
 }
 // console.log(student);
 // console.log(student.name);
 // console.log(student.laptop);
-console.log(student.laptop.brand);
+// console.log(student.laptop.brand);
+// console.log(student.laptop.brand.length);
+console.log(student.laptop.brand?.length);
