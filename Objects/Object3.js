4b22f51d878e6254dd3a89703c190d1c69de0f4d
Arvind Kharwal <karvind629@gmail.com>
2026-07-26 22:31:54 +0530

    26 July 2026

---
+++ Objects/Object3.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Arvind",
+    age: 44,
+    laptop1: {
+        cpu:'i7',
+        ram:8,
+        brand1:'Dell',
+    }
+}
+console.log(student.laptop.brand?.length);
