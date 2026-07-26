62743e719e9c71dd85deaa03aeca08cc01cf1628
Arvind Kharwal <karvind629@gmail.com>
2026-07-26 23:04:01 +0530

    26 July 2026

---
+++ Basics/Let1.js
@@ -0,0 +1,11 @@
+    function start(){
+        for(var i = 0;i<5;i++){
+            if(true){
+            let color = 'red';
+            }
+        }
+        // console.log(i)
+        console.log(color)
+    }
+
+    start();
