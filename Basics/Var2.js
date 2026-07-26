be806dbf0f03de321b8eba1aeaa10d29b4712ec4
Arvind Kharwal <karvind629@gmail.com>
2026-07-26 22:58:43 +0530

    26 July 2026

--- Basics/Var2.js
+++ Basics/Var2.js
@@ -1,8 +1,8 @@
     function start(){
-        for(let i = 0;i<5;i++){
+        for(var i = 0;i<5;i++){
         console.log(i)
         }
-        // console.log(i)
+        console.log(i)
     }
 
     start();
