446f312adcef99eb9e97e15b88345819cc1efa12
Arvind Kharwal <karvind629@gmail.com>
2026-07-29 22:20:38 +0530

    29 July 2026

--- Array/Array1.js
+++ Array/Array1.js
@@ -1,2 +1,4 @@
 let colors = ['red','blue'];
 console.log(colors);
+colors[2] = 'green'
+console.log(colors);
--- Basics/global.js
+++ Basics/global.js
@@ -3,6 +3,9 @@
 var color = 'red';
 let age = 30;
 
+console.error("Arvind Error")
+console.warn("Arvind Warning")
+
 function sayHi(){
     console.log('Hello Arvind');
 }
