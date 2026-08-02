b736fb86db6888df0986e90c41e6fba825a02c24
Arvind Kharwal <karvind629@gmail.com>
2026-08-02 22:52:50 +0530

    2 August 2026

---
+++ Conditional/switch.js
@@ -0,0 +1,13 @@
+let v = "A";
+switch(v)
+{
+    case "B":
+        console.log('One');
+        break;
+    case "A":
+        console.log('Two');
+        break;
+    default:
+        console.log('Default');
+
+}
