a81edd9af4969ef2c0d620c28f600427ace4c839
Arvind Kharwal <karvind629@gmail.com>
2026-08-12 22:39:57 +0530

    12 August 2026

---
+++ Basics/switch.js
@@ -0,0 +1,15 @@
+let ch = "Hello";
+switch(ch)
+{
+    case 1:
+        console.log("One");
+        break;
+    case 5.5:
+        console.log("five");
+        break;
+    case "Hello":
+        console.log("Hello");
+        break;
+    default:
+        console.log("default");
+}
