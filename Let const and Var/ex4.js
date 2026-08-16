b7548a0e73bb3a62b759f57b386a765330019a2c
Arvind Kharwal <karvind629@gmail.com>
2026-08-16 22:51:23 +0530

    16 August 2026

--- Let const and Var/ex4.js
+++ Let const and Var/ex4.js
@@ -1,2 +1,13 @@
-console.log(name);
-var name = "Arvind";
+// Hoisting of var 
+console.log(name1);
+var name1 = "Arvind";
+
+// Hoisting of let 
+
+console.log(name2);
+let name2 = "Arvind";
+
+// Hoisting of const
+
+console.log(name3);
+const name3 = "Arvind";
