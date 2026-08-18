510c7b75fb5cc74bb413d8ac73223e28c4cdf86b
Arvind Kharwal <karvind629@gmail.com>
2026-08-18 16:14:31 +0530

    18 August 2026

--- first class citizens/fcc1.js
+++ first class citizens/fcc1.js
@@ -2,5 +2,8 @@ function first(a,b){
     return a+b;
 }
  let res = first;
- let r = res(10,20)
- console.log(r);
+ let r1 = res(10,20)
+ console.log(r1);
+
+ let r2 = first(30,40);
+ console.log(r2);
