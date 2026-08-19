edc8f239371177a40cf269bc35ff3f213d62cd64
Arvind Kharwal <karvind629@gmail.com>
2026-08-19 21:02:38 +0530

    19 August 2026

---
+++ Let const and Var/test3.js
@@ -0,0 +1,11 @@
+function example(){
+    var name= "Arv";
+    console.log(name);
+    if(true){
+        var name= "Duggu";
+        console.log(name);
+    }
+    console.log(name);
+}
+
+example();
