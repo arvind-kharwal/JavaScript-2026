f232f4de9f43a6adda11ba05c5f6c8fdbfa40bfe
Arvind Kharwal <karvind629@gmail.com>
2026-08-20 22:34:04 +0530

    20 August 2026

--- Objects/complex.js
+++ Objects/complex.js
@@ -4,7 +4,11 @@ const person = {
     laptop:{
         cpu:'i7',
         ram:'8gb',
-        brand:"Dell"
+        brand1:"Dell & system"
     }
 };
 console.log(person);
+console.log(person.name);
+console.log(person.laptop.ram);
+// console.log(person.laptop.brand);
+console.log(person.laptop.brand1.length);
