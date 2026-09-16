ba1c1d925cda4891b7f583843caa930b01c4dc87
Arvind Kharwal <karvind629@gmail.com>
2026-09-16 22:43:35 +0530

    16 September 2026

---
+++ Callback function/second0.js
@@ -0,0 +1,12 @@
+const evennum = (arr)=>{
+    for(let i=0;i<arr.length;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            console.log(arr[i]);
+        }
+    }
+};
+
+a = [1,2,3,4,5,6,7,9];
+evennum(a);
