e81c5dd0b408a86ce3f604cd0e41ad9cda47d3eb
Arvind Kharwal <karvind629@gmail.com>
2026-09-16 23:27:05 +0530

    16 September 2026

---
+++ forEach/eight.js
@@ -0,0 +1,4 @@
+const numbers = [10, 20, 30];
+numbers.forEach(function(element, index, array) {
+    console.log(`Element: ${element}, Index: ${index}, Array: ${array}`);
+});
---
+++ forEach/seven.js
@@ -0,0 +1,6 @@
+const num = ['a','b','c','d','e'];
+const values = num.forEach((n)=>{
+    return(n);
+});
+
+console.log(values);
