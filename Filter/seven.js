7cdbffa8be2996ac7749e9aa6968a295d156359e
Arvind Kharwal <karvind629@gmail.com>
2026-09-16 16:30:44 +0530

    16 September 2026

---
+++ Filter/seven.js
@@ -0,0 +1,32 @@
+const books = [
+    {
+        title:'Let us C',
+        price:499,
+        author:'Y.Kanitkar'
+    },
+    {
+        title:'Java Programming',
+        price:699,
+        author:'James Gosling'
+    },
+    {
+        title:'Python',
+        price:799,
+        author:'Guido Von Rossum'
+    },
+    {
+        title:'Let us C++',
+        price:849,
+        author:'Bjarne Stroustrup'
+    },
+    {
+        title:'JavaScript',
+        price:1499,
+        author:'Brendan Eich'
+    },
+];
+
+userbooks = books.filter((b)=>{
+    return b.price>500;
+});
+console.log(userbooks);
