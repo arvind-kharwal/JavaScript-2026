5ed45540edcafad9bf5f999c1f71b79ad49ed166
Arvind Kharwal <karvind629@gmail.com>
2026-09-20 16:07:27 +0530

    20 September 2026

--- Prototype/second1.js
+++ Prototype/second1.js
@@ -1,9 +1,16 @@
 let Person = {
     name: "Arvind",
     age: 40,
-    greet: function(){
+    greet: function() {
         console.log(`${this.name} and ${this.age}`);
     }
 };
-let myarr = ['Arvind','Yuvaan'];
-myarr.greet();
+let myarr = ['Arvind', 'Yuvaan'];
+Person.greet();   // Output: Arvind and 40
+
+// Option 2: Add greet to Array prototype
+Array.prototype.greet = function() {
+    console.log(`Array contents: ${this.join(', ')}`);
+};
+
+myarr.greet();    // Output: Array contents: Arvind, Yuvaan
--- Prototype/second2.js
+++ Prototype/second2.js
@@ -1,9 +1,13 @@
 let Person = {
     name: "Arvind",
     age: 40,
-    greet: function(){
+    greet: function() {
         console.log(`${this.name} and ${this.age}`);
     }
 };
-let myarr = ['Arvind','Yuvaan'];
-myarr.greet();
+let myarr = ['Arvind', 'Yuvaan'];
+Array.prototype.show = function() {
+    console.log('Show function');
+};
+Person.greet();   
+myarr.show();    
