516867caea4df428a0670c55d33330b509d654f2
Arvind Kharwal <karvind629@gmail.com>
2026-09-20 11:57:01 +0530

    20 September 2026

---
+++ Prototype/proto2.js
@@ -0,0 +1,20 @@
+function Person(name,age,sal){
+    this.name = name;
+    this.age = age;
+    this.sal = sal;
+}
+Person.increaseSalary = function(inr){
+    this.sal+=inr;
+}
+Person.display= function(){
+    console.log(`Name: ${this.name}`);
+    console.log(`Salary after hike:${this.sal}`);
+    
+}
+
+const P1 = new Person("Arvind",40,10000);
+P1.increaseSalary(2300);
+P1.display();
+const P2 = new Person("Yuvaan",5,0)
+P2.increaseSalary(233);
+P2.display();
