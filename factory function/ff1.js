6655628dc9574ef86443b00d182ca3df509a8486
Arvind Kharwal <karvind629@gmail.com>
2026-09-25 12:12:42 +0530

    25 September 2026

---
+++ factory function/ff1.js
@@ -0,0 +1,19 @@
+function createPerson(fname,lname){
+    return {
+        firstname:fname,
+        lastname:lname,
+        getFullName(){
+            return fname+ " "+lname;
+        }
+    }
+}
+let p1 = createPerson("Arvind","Kumar");
+console.log(p1.getFullName());
+console.log(p1.firstname);
+console.log(p1.lastname);
+
+let p2 = createPerson("Yuvaan","Singh");
+console.log(p2.getFullName());
+
+
+
